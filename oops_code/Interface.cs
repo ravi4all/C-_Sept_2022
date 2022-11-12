314593ee5d8513a01e1cef6f103c1c2d22d231e9
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-11-12 17:53:58 +0530

    Add files via upload

---
+++ oops_code/Interface.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+interface IPlayer
+{
+    void walk();
+    void run();
+    void jump();
+}
+
+interface IPlayerAction
+{
+    void punch();
+    void kick();
+    void superKick();
+}
+
+abstract class PlayerExtraPower : IPlayer
+{
+    abstract public void disappear();
+    public void jump()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void run()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void walk()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class King : PlayerExtraPower, IPlayerAction
+{
+    public override void disappear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void kick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void punch()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public void superKick()
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
+class Eddy : IPlayer, IPlayerAction
+{
+    public void jump()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void kick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void punch()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void run()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void superKick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void walk()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+namespace OOPS_Introduction
+{
+    class InterfaceDemo
+    {
+
+    }
+}
---
+++ oops_code/Overloading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPS_Introduction
+{
+    class Overloading
+    {
+        void show(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+        void show(char x)
+        {
+            Console.WriteLine(x);
+        }
+
+        void show(char x, int y)
+        {
+            Console.WriteLine(x);
+        }
+
+        public static void Main()
+        {
+            Overloading obj = new Overloading();
+            obj.show(10);
+            obj.show('b');
+            obj.show('h',6);
+        }
+    }
+}
