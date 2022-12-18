e429d99133fec9cc4c893ca38d2d2db5fba36896
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2022-12-18 17:46:12 +0530

    Add files via upload

--- ClockScript.cs
+++ ClockScript.cs
@@ -5,29 +5,20 @@ using System;
 
 public class ClockScript : MonoBehaviour
 {
-
+    float hoursToDegree = -6f;
     [SerializeField]
-    // Transform NiddlePivot, SecondsPivot;
-    Transform NiddlePivot;
+    Transform niddlePivot;
 
-    void Awake () {
-        // Debug.Log(DateTime.Now.Hour);
-        DateTime time = DateTime.Now;
-        NiddlePivot.localRotation = Quaternion.Euler(0,0,-30 * time.Hour);
-        // MinutePivot.localRotation = Quaternion.Euler(0,0,-30 * DateTime.Now.Minute);
-        // SecondPivot.localRotation = Quaternion.Euler(0,0,-30 * DateTime.Now.Second);
+    // void Awake() {
+        // Debug.Log(DateTime.Now);
+        // niddlePivot.localRotation = Quaternion.Euler(0,0,-30);
+        // niddlePivot.localRotation = Quaternion.Euler(0,0, hoursToDegree * DateTime.Now.Hour);
+    // }
 
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        var time  = DateTime.Now;
+        // niddlePivot.localRotation = Quaternion.Euler(0,0, hoursToDegree * DateTime.Now.Hour);
+        niddlePivot.localRotation = Quaternion.Euler(0,0, 
+                                    hoursToDegree * time.Second);
     }
 }
